596ec51eea12546080b3efeaaba9aac5bdb7e3cd
rushikeshchaudhari01 <121921658+rushikeshchaudhari01@users.noreply.github.com>
2024-01-19 18:09:43 +0530

    Add files via upload

---
+++ Claculater.java
@@ -0,0 +1,12 @@
+package simplecalulater;
+
+import java.awt.Frame;
+
+public class Claculater extends Frame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
